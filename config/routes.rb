44232fb824b793bf0b3ce8931b991a9f97980319
John Brunton <john_brunton@hotmail.co.uk>
2015-04-26 10:11:48 +0100

    #9 simplify route for report

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,7 @@ Rails.application.routes.draw do
       get 'epics'
     end
 
-    resources :reports do
-      collection do
-        get 'cycle_times'
-      end
-    end
+    get 'reports/cycle_times', to: 'reports#cycle_times'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
