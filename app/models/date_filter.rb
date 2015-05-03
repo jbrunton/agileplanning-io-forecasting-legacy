f049c43063a3edd3112e1f030603f3b7ded08010
John Brunton <john_brunton@hotmail.co.uk>
2015-05-03 09:24:49 +0100

    #19 add DateFilter

---
+++ app/models/date_filter.rb
@@ -0,0 +1,17 @@
+class DateFilter
+  def initialize(filter)
+    @date_ranges = filter.split(",").map do |x|
+      x.split("-").map{ |x| DateTime.parse(x.strip) }
+    end
+  end
+
+  def allow_date(date)
+    return true if @date_ranges.empty?
+    @date_ranges.select{ |range| range[0] <= date && date <= range[1] }.length > 0
+  end
+
+  def allow_issue(issue)
+    return true if @date_ranges.empty?
+    @date_ranges.select{ |range| range[0] <= issue.started && issue.completed <= range[1] }.length > 0
+  end
+end
