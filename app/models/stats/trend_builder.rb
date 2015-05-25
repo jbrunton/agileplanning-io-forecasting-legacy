082b1566ba717801f38a99c4cc0306de92c28b90
John Brunton <jbrunton@zipcar.com>
2015-05-25 20:17:52 +0100

    #66 partial implementation of TrendBuilder

--- app/models/stats/trend_builder.rb
+++ app/models/stats/trend_builder.rb
@@ -15,4 +15,23 @@ class Stats::TrendBuilder
       @map_block.call(item, value, 0)
     end
   end
+
+  def self.sample_size(series_size)
+    sample_size = (series_size * 0.2).to_i
+    sample_size = sample_size + 1 if sample_size.even?
+    [sample_size, 5].max
+  end
+
+  def self.pick_sample(series, index)
+    sample_size = self.sample_size(series.length)
+    start_index = [0, index - sample_size / 2 - 1].max
+    end_index = start_index + sample_size
+
+    while end_index > series.length
+      start_index = start_index - 1 if start_index > 0
+      end_index = end_index - 1
+    end
+
+    series[start_index..end_index - 1]
+  end
 end
