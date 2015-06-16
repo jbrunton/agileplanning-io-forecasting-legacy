c89c1017a5ec918e373820f7feb015009f5acf17
John Brunton <jbrunton@zipcar.com>
2015-06-16 19:33:45 +0100

    #66 added overlay albeit without tests

--- app/assets/javascripts/stats/cycle_time_chart.js
+++ app/assets/javascripts/stats/cycle_time_chart.js
@@ -33,6 +33,19 @@ CycleTimeChart.prototype.bind = function() {
 };
 
 CycleTimeChart.prototype.setSeries = function(cycleTimeSeries, wipSeries) {
+  // TODO: extract these functions
+  var bisectDate = d3.bisector(function(d) { return d.completed ? d.completed : d.date; }).left;
+
+  function cycleTimeTip(d, active) {
+    var context = { d: d, active: active };
+    return HandlebarsTemplates['epic_tooltip'](context);
+  }
+
+  function wipTip(d, active) {
+    var context = { d: d, active: active };
+    return HandlebarsTemplates['wip_tooltip'](context);
+  }
+
   this.cycleTimeSeries = cycleTimeSeries;
   this.wipSeries = wipSeries;
 
@@ -153,4 +166,132 @@ CycleTimeChart.prototype.setSeries = function(cycleTimeSeries, wipSeries) {
       .attr("class", "y axis")
       .attr("transform", "translate(" + chart.getClientWidth() + ",0)")
       .call(yWipAxis);
+
+
+  // overlay
+
+  $('.d3-tip').remove();
+
+  var focusActive = false;
+
+  var tip = d3.tip().attr('class', 'd3-tip')
+      .direction(function(d) {
+        var y = d.cycleTime ? chart._yCycleTimeScale(d.cycleTime) : chart._yWipScale(d.wip);
+        return y > chart.getClientHeight() / 2 ? 'n' : 's';
+      })
+      .offset(function(d) {
+        var y = d.cycleTime ? chart._yCycleTimeScale(d.cycleTime) : chart._yWipScale(d.wip);
+        return y > chart.getClientHeight() / 2 ? [-10, 0] : [10, 0];
+      })
+      .html(function(d) {
+        var html = "";
+        if (d.cycleTime) {
+          html += cycleTimeTip(d, focusActive);
+        } else {
+          html += wipTip(d, focusActive);
+        }
+        return html;
+      });
+  chart.svg.call(tip);
+
+  var overlay = chart.svg.append('rect')
+      .attr('class', 'overlay')
+      .attr('width', chart.getClientWidth())
+      .attr('height', chart.getClientHeight());
+
+  var focus = chart.svg.append('g').style('display', 'none');
+  focus.append('circle')
+      .attr('id', 'focusCircle')
+      .attr('r', 5);
+
+  overlay.on('click', function() { focusActive = false; tip.hide(); focus.style('display', 'none'); })
+      .on('mousemove', function() {
+        function getMouseXY(self) {
+          var mouse = d3.mouse(self);
+          return { x: mouse[0], y: mouse[1] };
+        }
+
+        function getCycleTimeXY(index) {
+          if (index < 0 || index >= cycleTimeSeries.length) {
+            return { x: -999999, y: -999999, data: d };
+          }
+          var d = cycleTimeSeries[index];
+          return { x: chart._xScale(d.completed), y: chart._yCycleTimeScale(d.cycleTime), data: d };
+        }
+
+        function getWipXY(index) {
+          if (index < 0 || index >= wipSeries.length) {
+            return { x: -999999, y: -999999, data: d };
+          }
+          var d = wipSeries[index];
+          return { x: chart._xScale(d.date), y: chart._yWipScale(d.wip), data: d };
+        }
+
+        function distanceSq(p0, p1) {
+          return Math.pow(p1.x - p0.x, 2) + Math.pow(p1.y - p0.y, 2);
+        }
+
+        function getClosestCycleTimeXY(p) {
+          var mouseDate = chart._xScale.invert(mouse.x);
+          var i = bisectDate(cycleTimeSeries, mouseDate); // returns the index to the current data item
+          var p0 = getCycleTimeXY(i - 1);
+          var p1 = getCycleTimeXY(i);
+
+          p0.dist = distanceSq(p0, mouse);
+          p1.dist = distanceSq(p1, mouse);
+
+          // work out which date value is closest to the mouse
+          return p0.dist > p1.dist ? p1 : p0;
+        }
+
+        function getClosestWipXY(p) {
+          var mouseDate = chart._xScale.invert(mouse.x);
+          var i = bisectDate(wipSeries, mouseDate); // returns the index to the current data item
+          var p0 = getWipXY(i - 1);
+          var p1 = getWipXY(i);
+
+          p0.dist = distanceSq(p0, mouse);
+          p1.dist = distanceSq(p1, mouse);
+
+          // work out which date value is closest to the mouse
+          return p0.dist > p1.dist ? p1 : p0;
+        }
+
+        var mouse = getMouseXY(this);
+        var pCycleTime = getClosestCycleTimeXY(mouse);
+        var pWip = getClosestWipXY(mouse);
+
+        if (focusActive) {
+          return;
+        }
+
+        if (pCycleTime.dist < pWip.dist && pCycleTime.dist < 2500) {
+          focus.select('#focusCircle')
+              .attr('cx', pCycleTime.x)
+              .attr('cy', pCycleTime.y)
+              .attr('class', 'cycle_time')
+              .on('click', function() {
+                focusActive = true;
+                tip.show(pCycleTime.data, target);
+              });
+          var target = d3.selectAll("circle.cycle_time").filter(function(d) { return d == pCycleTime.data; })[0][0];
+          tip.show(pCycleTime.data, target);
+          tip.attr('class', 'd3-tip cycle_time');
+          focus.style('display', null);
+        } else {
+          focus.select('#focusCircle')
+              .attr('cx', pWip.x)
+              .attr('cy', pWip.y)
+              .attr('class', 'wip')
+              .on('click', function() {
+                focusActive = true;
+                tip.show(pWip.data, target);
+              });
+
+          var target = d3.selectAll("circle.wip").filter(function(d) { return d == pWip.data; })[0][0];
+          tip.show(pWip.data, target);
+          tip.attr('class', 'd3-tip wip');
+          focus.style('display', null);
+        }
+      });
 };
