12d05b9342817fe96c1bf75179eb79541ca9616f
John Brunton <jbrunton@zipcar.com>
2015-08-08 16:07:41 +0100

    #144 rename 'interval' to 'confidence_level'

--- app/models/forecaster.rb
+++ app/models/forecaster.rb
@@ -6,12 +6,12 @@ class Forecaster
   def forecast_lead_times(opts)
     total = opts[:sizes].values.reduce(:+)
     opts = opts.merge(rank: total) # to ensure that we don't divide by WIP when total < WIP
-    @simulator.play(opts).map do |interval|
+    @simulator.play(opts).map do |confidence_level|
       if opts[:start_date]
-        expected_date = opts[:start_date] + interval[:actual_time].days
-        interval.merge!(expected_date: expected_date)
+        expected_date = opts[:start_date] + confidence_level[:actual_time].days
+        confidence_level.merge!(expected_date: expected_date)
       end
-      interval
+      confidence_level
     end
   end
 end
